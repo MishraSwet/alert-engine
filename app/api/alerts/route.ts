839d8575e3c58dd0e23fb7d85eae167b2a2fccf6
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-14 18:20:33 +0530

    Completed Logic For Alert Engine in the Backend

---
+++ app/api/alerts/route.ts
@@ -0,0 +1,37 @@
+import { WebSocket, WebSocketServer } from "ws";
+const clients: Map<string, WebSocket> = new Map<string, WebSocket>;
+const queue: Map<string, string[]> = new Map<string, string[]>;
+
+const server = new WebSocketServer({ port: 8080 })
+
+server.on('connection', (socket) => {
+    socket.on('identify', (clientdata: string) => {
+        const data = JSON.parse(clientdata);
+        const id = data.id;
+        if (clients.has(id)) {
+            if (queue.get(id))
+                queue.get(id)
+        }
+        else {
+            clients.set(id, socket);
+            queue.set(id, []);
+        }
+    })
+    socket.on('alert', (message: string) => {
+        const data = JSON.parse(message);
+        const text:string = data.text;
+        const rcvid:string = data.rcvid;
+        if (clients.get(rcvid)) {
+            clients.get(rcvid)?.send(text)
+        }
+        else {
+            queue.get(rcvid)?.push(text);
+        }
+    })
+    socket.on('close', () => {
+        clients.forEach((ws, id) => {
+            if (clients.get(id) === ws)
+                clients.delete(id)
+        })
+    })
+})
